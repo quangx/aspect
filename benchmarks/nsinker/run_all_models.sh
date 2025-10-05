aedc29edb93fb857828f6ab169933d6872c162ac
Quang Hoang <qxhmorrow@gmail.com>
2025-10-04 23:50:43 -0400

    initial commit

--- benchmarks/nsinker/run_all_models.sh
+++ benchmarks/nsinker/run_all_models.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for averaging in none "harmonic average"; do # arithmetic/geometric/harmonic average
-  for nsinkers in 1 4 8 12 16 20 24 28; do
+  for nsinkers in 1 4 8  16 ; do
     for viscosity in 1e4 1e6 1e8 1e10; do
-      for refinement in 3; do # 4 5 6 7
+      for refinement in 3; do  4 5 6 7
         echo "subsection Material model" > current.prm
         echo "  set Material averaging = $averaging" >> current.prm
         echo "  subsection NSinker" >> current.prm
@@ -19,7 +19,12 @@ for averaging in none "harmonic average"; do # arithmetic/geometric/harmonic ave
 	current_model="averaging${averaging}_nsinkers${nsinkers}_viscosity${viscosity}_refinement${refinement}"
         echo "set Output directory = output-${current_model}" >> current.prm
         echo "Starting ${current_model}"
-        cat nsinker.prm current.prm | mpirun -np 4 ./aspect --
+	echo "subsection Solver parameters">>current.prm
+	echo "subsection Stokes solver parameters">> current.prm
+	echo "set Stokes solver type = block GMG">> current.prm
+	echo "end">>current.prm
+	echo "end">>current.prm
+        cat nsinker.prm current.prm | mpirun -np  32 ./aspect --
       done
     done
   done
