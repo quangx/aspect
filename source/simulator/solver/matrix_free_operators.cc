6489a936daca51c734906c69f331c0d50370eb93
Quang Hoang <qxhmorrow@gmail.com>
2026-03-10 21:58:43 -0400

    initialize

--- source/simulator/solver/matrix_free_operators.cc
+++ source/simulator/solver/matrix_free_operators.cc
@@ -1023,6 +1023,8 @@ namespace aspect
   template class MatrixFreeStokesOperators::StokesOperator<dim,3,GMGNumberType>; \
   template class MatrixFreeStokesOperators::BTBlockOperator<dim,2,GMGNumberType>; \
   template class MatrixFreeStokesOperators::BTBlockOperator<dim,3,GMGNumberType>; \
+  template class MatrixFreeStokesOperators::BBlockOperator<dim,2,GMGNumberType>;\
+  template class MatrixFreeStokesOperators::BBlockOperator<dim,3,GMGNumberType>;\
   template class MatrixFreeStokesOperators::MassMatrixOperator<dim,1,GMGNumberType>; \
   template class MatrixFreeStokesOperators::MassMatrixOperator<dim,2,GMGNumberType>; \
   template struct MatrixFreeStokesOperators::OperatorCellData<dim, GMGNumberType>;
