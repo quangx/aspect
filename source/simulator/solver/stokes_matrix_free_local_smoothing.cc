8a9d7b55b9e24c8a9380151da9d527d904efbe15
Quang Hoang <qxhmorrow@gmail.com>
2026-07-20 23:57:33 -0400

    fix and use CG

--- source/simulator/solver/stokes_matrix_free_local_smoothing.cc
+++ source/simulator/solver/stokes_matrix_free_local_smoothing.cc
@@ -1759,7 +1759,7 @@ namespace aspect
         // instead of requiring FGMRES, greatly lowing the memory requirement of the solver.
         if (this->get_parameters().stokes_krylov_type == Parameters<dim>::StokesKrylovType::gmres)
           {
-            SolverGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>
+            SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>
             solver(solver_control_cheap, mem,
                    SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>::
                    AdditionalData(this->get_parameters().stokes_gmres_restart_length+2
