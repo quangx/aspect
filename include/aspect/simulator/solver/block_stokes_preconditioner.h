6477d5b189df57c18e4bec2d9f60eb02c57df341
Quang Hoang <qxhmorrow@gmail.com>
2026-09-05 19:31:44 -0400

    full solve A

--- include/aspect/simulator/solver/block_stokes_preconditioner.h
+++ include/aspect/simulator/solver/block_stokes_preconditioner.h
@@ -140,7 +140,7 @@ namespace aspect
       // Either solve with the top left block
       // or just apply one preconditioner sweep (for the first few
       // iterations of our two-stage outer GMRES iteration)
-      if (do_solve_A == true)
+      if (do_solve_A == true || do_solve_A == false)
         {
           SolverControl solver_control(10000, src.l2_norm() * solver_tolerance);
           PrimitiveVectorMemory<VectorType> mem;
@@ -180,11 +180,11 @@ namespace aspect
                                                                src.get_mpi_communicator());
             }
         }
-      else
-        {
-          preconditioner.vmult (dst, src);
-          n_iterations_ += 1;
-        }
+      // else
+      //   {
+      //     preconditioner.vmult (dst, src);
+      //     n_iterations_ += 1;
+      //   }
     }
 
 
