8d89156a82b8e8ca31c154faf84c7814d9020679
Quang Hoang <qxhmorrow@gmail.com>
2026-09-11 13:55:28 -0400

    switch back to A block vmult and gmres

--- source/simulator/solver/stokes_matrix_free_local_smoothing.cc
+++ source/simulator/solver/stokes_matrix_free_local_smoothing.cc
@@ -1901,11 +1901,11 @@ namespace aspect
         // instead of requiring FGMRES, greatly lowing the memory requirement of the solver.
         if (this->get_parameters().stokes_krylov_type == Parameters<dim>::StokesKrylovType::gmres)
           {
-            SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>
+            SolverGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>
             solver(solver_control_cheap, mem,
-                   SolverFGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>::
+                   SolverGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>>::
                    AdditionalData(this->get_parameters().stokes_gmres_restart_length+2
-                                  /*,true*/));
+                                  ,true));
 
             solver.solve (stokes_matrix,
                           solution_copy,
